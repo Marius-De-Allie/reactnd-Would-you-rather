1680f28a8bdd5983c8e5ebb126f38731d9e150b1
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-20 17:54:42 -0400

    Declare logger middleware function within '/src/middleware/logger.js' file

---
+++ src/middleware/logger.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Logger middleware.
+const logger = (store) => (next) => (action) => {
+    console.group(action.type);
+    console.log('The action: ', action);
+    const returnedValue = next(action);
+    console.log('The new state: ', store.getState());
+    console.groupEnd();
+    return returnedValue;
+};
