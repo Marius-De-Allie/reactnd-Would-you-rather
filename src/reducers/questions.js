3ef1567894f4e541ed1a60f387da532802f24d55
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-20 16:15:10 -0400

    Declare questions reducer setting default state to an empty object

---
+++ src/reducers/questions.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Questions reducer, has questions has default state of an empty object {}.
+const questions = (state = {}, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
