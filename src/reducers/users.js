65534bcc9f2471d009930eb111bec2be4dd7bb21
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-20 16:51:00 -0400

    Export users reducer as default export from '/src/reducers/users.js file'

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// Users reducer, has questions has default state of an empty object {}.
 const users = (state = {}, action) => {
     switch(action.type) {
         default:
             return state;
     }
 };
+
+export default users;
