6ece3cbbea17900cab1e968c258338176ddbe004
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-20 17:21:57 -0400

    Call combineReducers function passing in the questions users and authedUser reducers as object property values

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,10 @@ import { combineReducers } from redux;
 import questions from './questions';
 import users from './users';
 import authedUser from './authedUser';
+
+// Call combinereducers passing in all 3 reducers as object property values.
+export default combineReducers({
+    questions,
+    users,
+    authedUser
+});
