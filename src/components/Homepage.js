88b559c9551ec15f52f54ea678bb6b9eef9cd910
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-21 15:44:11 -0400

    Declare mapStateToProps function to map questions, users and authedUser state to component props in Homepage.js file

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -11,3 +11,10 @@ class Homepage extends Component {
         );
     }
 };
+
+// Which state from the redux store will be passed to his component as props.
+const mapStateToProps = (state) => ({
+    questions: state.questions,
+    users: state.users,
+    authedUser: state.authedUser
+}); 
