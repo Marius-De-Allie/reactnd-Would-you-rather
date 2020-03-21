cefe12252fd55884b1083fcd866f70b793febefd
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-21 15:40:59 -0400

    Declare Homepage class based component in Homepage.js file

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,3 +1,13 @@
 'use strict';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                Home Page
+            </div>
+        );
+    }
+};
