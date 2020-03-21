15e7778cd9e97a53c7fcf2a5feaaa0bc80d6be63
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-20 22:37:34 -0400

    Import middleware into index.js file from '/src/middleware/index.js file default export'

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './components/App';
 // Import reducers from combineReducers function call.
 import reducer from './reducers';
+// Import middleware from applyMiddleware function call.
+import middleware from './middleware';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
