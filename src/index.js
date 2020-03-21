455638270e40c36a1bea463c91b08f89375dcf51
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-20 22:55:03 -0400

    Import Provider component into index.js file from react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 // Import reducers from combineReducers function call.
