feda188616f25339e51a3da8aa737ae8f07ce152
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-22 16:13:59 -0400

    Create thunk action creator handleReceiveQuestions
    Create thunk action creator handleReceiveQuestions which calls _getQuestions function and then dispatches receiveQuestions action with the result from _getQuestions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,5 @@
 'use strict';
+// Import getQuestions async API function.
 import _getQuestions from '../../_DATA';
 const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 
@@ -6,5 +7,18 @@ const receiveQuestions = (questions) => ({
     type: RECEIVE_QUESTIONS,
     questions
 });
+
+// Thunk action creator to request all questions in database.
+const handleReceiveQuestions = () => {
+    return (dispatch) => {
+        // Request all questions from database.
+        _getQuestions()
+        // dispatch receivequestions action with result from _getQuestions async request.
+        .then((questions) => {
+            console.log(questions);
+            dispatch(receiveQuestions(questions))
+        })
+    }
+};
 
 export {RECEIVE_QUESTIONS, receiveQuestions}; 
