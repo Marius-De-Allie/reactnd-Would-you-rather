b86186a28bee9d1059d4bfeb05c591cd9fc1fd21
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-22 18:56:16 -0400

    Import RECEIVE_QUESTIONS action type into 'src/reducers/questions' reducer from 'src/actions/questions.js' file

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,5 @@
 'use strict';
+import { RECEIVE_QUESTIONS } from '../actions/questions';
 
 // Questions reducer, has default state of an empty object {}.
 const questions = (state = {}, action) => {
