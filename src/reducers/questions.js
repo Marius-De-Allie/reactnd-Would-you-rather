d0faf242abeb2e0324ed876522f92afe4c23d7f4
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-22 19:04:20 -0400

    Add a case for RECEIVE_QUESTIONS action type to questions reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -4,6 +4,8 @@ import { RECEIVE_QUESTIONS } from '../actions/questions';
 // Questions reducer, has default state of an empty object {}.
 const questions = (state = {}, action) => {
     switch(action.type) {
+        case RECEIVE_QUESTIONS:
+            return {...state, ...action.questions}
         default:
             return state;
     }
