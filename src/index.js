ecfeb483c5ada2f0bc1be412ef382a506f2b27bb
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-23 00:32:34 -0400

    Import BrowserRouter into index.js file from react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 // Import reducers from combineReducers function call.
