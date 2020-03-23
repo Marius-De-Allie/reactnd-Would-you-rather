97615d9d65dfdc6bddd10dffab82fc75e3c795a3
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-23 00:20:55 -0400

    Declare Nav functional component within Nav.js file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+const Nav = () => (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Unanswered</li>
+            <li>Answered</li>
+            <li>Leaderboard</li>
+        </ul>
+    </div>
+);
+
