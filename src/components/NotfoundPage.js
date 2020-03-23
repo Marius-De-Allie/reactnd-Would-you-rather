9ba0e73e5bfb9f0f0da1a530afcf16391f8b42c9
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-23 15:00:00 -0400

    Add Link component to NotfoundPage returned JSX elements, Link to property points to homepage at '/'

--- src/components/NotfoundPage.js
+++ src/components/NotfoundPage.js
@@ -4,5 +4,6 @@ import { Link } from 'react-router-dom';
 const NotfoundPage = () => (
     <div>
         <h3>404. Page not found!</h3>
+        <p>Return<Link to="/">Home</Link></p>
     </div>
 );
