61e468c0614a616df96218790dd28c0c2e9ba5a2
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-22 22:08:59 -0400

    Declare QuestionForm class based component which renders two radio inputs and a submit button

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,2 +1,20 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+class QuestionForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Would you rather</h1>
+                <div className="question-one">
+                    <input type="radio" name="question" />
+                </div>
+                <div className="question-two">
+                    <input type="radio" name="question" />
+                </div>
+                <button>Submit</button>
+            </div>
+        );
+    }
+};
+
