3b7443e0bf831c295abc1378fd72518994b87bc9
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-23 23:30:51 -0400

    Declare UnansweredList functional component within '/src/components/UnansweredList.js' file

---
+++ src/components/UnansweredList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UnansweredList = () => (
+    <div>
+        <h2>Unanswered Questions</h2>
+        <ul>
+            Question Items
+        </ul>
+    </div>
+);
