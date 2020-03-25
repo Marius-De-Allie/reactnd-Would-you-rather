e58ed3bdaee1883b9c236e59bfe4ad4fb57f4947
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-24 23:06:32 -0400

    Update import of SET_AUTHED_USER VARIABLE in authedUser reducer to a named import

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,4 +1,4 @@
-import SET_AUTHED_USER from '../actions/authedUser';
+import { SET_AUTHED_USER } from '../actions/authedUser';
 
 const authedUser = (state = null, action) => {
     switch(action.type) {
