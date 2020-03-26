96e00c3bfb0b1b9f8bbfb0c52000f59e62331edb
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-25 20:13:01 -0400

    Update mapStatetoProps function within UnansweredList component to return a new property UnansweredIds
    Update mapStatetoProps function within UnansweredList component to return a new property UnansweredIds, this property's value is equal to an array of all the ids of the questions the usered answered in the current pool of questions

--- src/components/UnansweredList.js
+++ src/components/UnansweredList.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const UnansweredList = () => (
-    <div>
+const UnansweredList = (props) => {
+    console.log(props);
+    return (
+        <div>
         <button><h2>Unanswered Questions</h2></button>
         <ul>
             Question Items
         </ul>
     </div>
-);
+    )
+};
+
 
 const mapStateToProps = (state) => ({
+    UnansweredIds: Object.keys(state.questions).filter(question => !Object.keys(state.users[state.authedUser].answers)
+        .includes(question)
+    ),
     questions: state.questions,
     users: state.users,
     authedUser: state.authedUser
