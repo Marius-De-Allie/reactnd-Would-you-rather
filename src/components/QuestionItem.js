b63041fb8730cadb9428bf8a3b6df37056909808
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-25 22:02:04 -0400

    Pass question.optionOne.text prop into QuestionItem rendered JSX

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -8,7 +8,7 @@ const QuestionItem = (props) => (
             <div>Avatar Image</div>{/*Convert to img element*/}
             <div className="question-content">
                 <h3>Would you Rather</h3>
-                <p>...question text...</p>
+                <p>...{[props.question.optionOne.text]}...</p>
                 <button>View Poll</button>
             </div>
         </div>
