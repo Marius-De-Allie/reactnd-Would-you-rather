a1b0136d7776ed9e345550d36102a5178cb0f8f0
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-26 11:38:02 -0400

    Declare onPollBtnClick function within QuestionItem component

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const QuestionItem = (props) => (
-    <div>
-        <h3>{props.author.name} asks:</h3>
+const QuestionItem = (props) => {
+    const onPollBtnClick = (evt, questionId) => {
+        evt.preventDefault();
+        
+    };
+    return (
         <div>
-            <div>Avatar Image</div>{/*Convert to img element*/}
-            <div className="question-content">
-                <h3>Would you Rather</h3>
-                <p>...{[props.question.optionOne.text]}...</p>
-                <button>View Poll</button>
+            <h3>{props.author.name} asks:</h3>
+            <div>
+                <div>Avatar Image</div>{/*Convert to img element*/}
+                <div className="question-content">
+                    <h3>Would you Rather</h3>
+                    <p>...{[props.question.optionOne.text]}...</p>
+                    <button>View Poll</button>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
+
 
 const mapStateToProps = ({questions, users, authedUser}, {id}) => {
     const question = questions[id];
