9ecc411e905001cbd8d519e9c68c78b558f3aeb2
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-26 14:35:31 -0400

    Declare handleAnswerQuestion thunk action creator in shared.js file, action creator calls _saveQuestionAnswer async function

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,1 +1,8 @@
 import {_saveQuestionAnswer } from '../_DATA';
+
+const handleAnswerQuestion = (authedUser, qid, answer) => {
+    return (dispatch) => {
+        return _saveQuestionAnswer(authedUser, qid, answer)
+        .then((result) => console.log(result));
+    }
+};
