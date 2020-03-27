63932b31f263c2f51b2e1fa08f35992532a49a4d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-26 22:11:47 -0400

    Add new case ADD_ANSWER to users reducer, to add the new answer property to the users state object

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -7,6 +7,17 @@ const users = (state = {}, action) => {
     switch(action.type) {
         case RECEIVE_USERS:
             return {...state, ...action.users}
+        case ADD_ANSWER:
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],
+                    answers: {
+                        ...state[authedUser].answers,
+                        [action.qid]: answer
+                    }
+                }
+            }
         default:
             return state;
     }
