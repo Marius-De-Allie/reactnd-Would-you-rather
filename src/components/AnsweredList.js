a369ce5f628946bccedd5761112d205e99431c23
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-02 00:29:24 -0400

    Declare authedUserAnswerIds variable within AnsweredList compoent to hold an array of ids for all questions answered by the currently logged in user

--- src/components/AnsweredList.js
+++ src/components/AnsweredList.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import QuestionsItem from './QuestionItem';
 
-const AnsweredList = (props) => (
-    <div>
-        <ul>
-            {props.answeredIds.map(id => (
-                <li key={id}>
-                    <QuestionsItem id={id}/>
-                </li>
-            ))}
-        </ul>
-    </div>
-);
+const AnsweredList = (props) => {
+    // Array of question ids for all questions answered by currently authenticated user.
+    const authedUserAnswerIds = Object.keys(this.props.users[this.props.authedUser].answers);
+    return (
+        <div>
+            <h3>Answered Questions</h3>
+            <ul>
+                {props.answeredIds.map(id => (
+                    <li key={id}>
+                        <QuestionsItem id={id}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+};
 
 
 const mapStateToProps = (state) => ({
-    allQuestionIds: Object.keys(state.questions).sort((a,b) => state.questions[b].timestamp - state.questions[a].timestamp),
+    allQuestionIds: Object.keys(state.questions).sort((a, b) => state.questions[b].timestamp - state.questions[a].timestamp),
     questions: state.questions,
     users: state.users,
     authedUser: state.authedUser
