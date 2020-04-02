b4ea95bb2c7c558495221d9177d2eaeb2044dc72
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-02 15:49:52 -0400

    Declare renderUI function within PollDetails component, renderUI conditally renders either answered or unanswered question item

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -4,7 +4,21 @@ import UnanPollItem from './UnaPollItem';
 import AnsPollItem from './AnsPollItem';
 
 const PollDetails = (props) => {
-    
+    renderUI = () => {
+        // Check whether user is logged in.
+        if(this.props.authedUser !== null) {
+            // Create an array of the currently logged in user's answer IDs.
+            const authedUserAnswerIds = Object.keys(this.props.users[this.props.authedUser].answers);
+            return (
+                <React.Fragment>
+                    {authedUserAnswerIds.includes(this.props.pollId) ? <AnsPollItem pollid={this.props.pollId}/> :
+                    <UnanPollItem pollid={this.props.pollId} />}
+                </React.Fragment>
+            );
+        } else {
+            return <h3>Please login at the top right to view poll details</h3>
+        }
+    };
     return (
         <div>
             
