f4682ca383db068fe825b8dce1037cb0f29276ae
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-03 00:51:43 -0400

    Add new case to questions reducer for ADD_VOTE action type, this reducer will add id of currently logged in user to respective votes array in questions store state when poll is voted on

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -6,6 +6,17 @@ const questions = (state = {}, action) => {
     switch(action.type) {
         case RECEIVE_QUESTIONS:
             return {...state, ...action.questions}
+        case ADD_VOTE:
+            return {
+                ...state,
+                [action.qid] : {
+                    ...state[action.qid],
+                    [action.answer] : {
+                        ...state[action.qid][action.answer],
+                        votes: state[action.qid][action.answer].votes.includes(action.authedUser) ? [...state[action.qid][action.answer].votes] : state[action.qid][action.answer].votes.concat([action.authedUser])
+                    }
+                }
+            }
         default:
             return state;
     }
