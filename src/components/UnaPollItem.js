a4602bcd7fb9dba52eb644783258d636f0926a6d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-02 23:08:48 -0400

    Import addVote action creator into '/src/components/UnaPollItem.js' file

--- src/components/UnaPollItem.js
+++ src/components/UnaPollItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NotfoundPage from './NotfoundPage';
 import { addAnswer } from '../actions/users';
+import { addVote } from '../actions/questions';
 
 const UnaPollItem = (props) => {
     onFormSubmit = (evt) => {
