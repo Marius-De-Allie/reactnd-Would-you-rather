e940394eaf1d7a9fe129295fc8c8d4c1ee4ef63b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-05 18:04:58 -0400

    Conditionally render poll details within renderUI function in AnsPollItem component

--- src/components/AnsPollItem.js
+++ src/components/AnsPollItem.js
@@ -11,8 +11,32 @@ const AnsPollItem = (props) => {
         // Calculate number votes for selected poll's 2nd option.
         const optionTwoVotes = questions[pollid].optionTwo.votes.length;
 
-
-
+        if(questions[pollid] === undefined) {
+            return <h3>No poll with this ID exists, please try again</h3>
+        } else {
+            return (
+                <React.Fragment>
+                    <h3>Would you Rather</h3>
+                    <p>{users[questions[pollid].author].name} asks:</p>
+                    <ul>
+                        <li>
+                            {questions[pollid].optionOne.text}
+                            {questions[pollid].optionOne.votes.includes(users[authedUser]) && <span> your selection</span>}
+                        </li>
+                        <p>{questions[pollid].optionOne.votes.length} {optionOneVotes !== 1 ? 'votes' : 'vote'} / 
+                            {Math.round(optionOneVotes / totalVotes * 100)}%
+                        </p>
+                        <li>
+                            {questions[pollid].optionTwo.text}
+                            {questions[pollid].optionTwo.votes.includes(users[authedUser]) && <span> your selection</span>}
+                        </li>
+                        <p>{questions[pollid].optionTwo.votes.length} {optionTwoVotes !== 1 ? 'votes' : 'vote'} / 
+                            {Math.round(optionTwoVotes / totalVotes * 100)}%
+                        </p>
+                    </ul>
+                </React.Fragment>
+            );
+        }
     };
     
     return (
