b7b6a8a3400e1c104aa92e7fdb76bbd807995bb3
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-05 17:48:53 -0400

    Declare optionOneVotes variable within renderUI function to hold total number of votes for selected poll's first option

--- src/components/AnsPollItem.js
+++ src/components/AnsPollItem.js
@@ -6,7 +6,8 @@ const AnsPollItem = (props) => {
         const {users, questions, authedUser, pollid} = props;
         // Calculate total number votes for selected question.
         const totalVotes = questions[pollid].optionOne.votes.length + questions[pollid].optionTwo.votes.length;
-        
+        // Calculate number votes for selected poll's 1st option.
+        const optionOneVotes = questions[pollid].optionOne.votes.length;
 
     };
     
