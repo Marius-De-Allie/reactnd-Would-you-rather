0d58b33bcaceb252e8d1acc5dadc6034329e2a98
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-06 14:42:34 -0400

    Import UserSelector component into '/src/components/Nav.js' file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import UserSelector from './UserSelector';
 
 const Nav = props => {
     console.log(props);
