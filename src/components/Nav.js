4b59c79eb4732c768289a8bac61c2b1b89974c6d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-06 14:25:37 -0400

    Declare mapStateToProps function within Nav component, function returns authedUser and users properties

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Nav = () => (
-    <div>
-        <div className="navlinks">
-            <ul>
-                <li><NavLink to="/" exact>Home</NavLink></li>
-                <li><NavLink to="/add">Add Poll</NavLink></li>
-                <li><NavLink to="/leaderboard">Leaderboard</NavLink></li>
-            </ul>
+const Nav = () => {
+
+    return (
+        <div>
+            <div className="navlinks">
+                <ul>
+                    <li><NavLink to="/" exact>Home</NavLink></li>
+                    <li><NavLink to="/add">Add Poll</NavLink></li>
+                    <li><NavLink to="/leaderboard">Leaderboard</NavLink></li>
+                </ul>
+            </div>
+            <div className="user-auth">
+        
+            </div>
         </div>
-    </div>
-);
+    );
+};
+
+const mapStateToProps = ({authedUser, users}) => ({
+    authedUser,
+    users
+});
 
 export default Nav;
