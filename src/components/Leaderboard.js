d947a84e534227a6a27a13fefe73b6931e4eb534
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-06 23:46:10 -0400

    Declare userTotals array in mapStateToProps function within Leaderbord component, array elements are an object for each user which has a property for userId and total questions answered and created for each user

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -10,5 +10,13 @@ const Leaderboard = props => {
 };
 
 const mapStateToProps = ({users, authedUser}) => {
+    // Return a new array that has an object for each user's id and their total for created and answered questions
+    let userTotals = Object.keys(users).map(el => {
+        return {
+            userId: el,
+            total: (users[el].questions.length + Object.keys(users[el].answers).length)
+        }
+    });
+
 
 };
