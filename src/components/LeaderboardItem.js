0e6a3fa4428410a53b94d1c0e37f3093478441e6
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-06 22:29:16 -0400

    Declare LeaderboardItem component

--- src/components/LeaderboardItem.js
+++ src/components/LeaderboardItem.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const LeaderboardItem = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>name</td>
+                <td>answered</td>
+                <td>asked</td>
+            </tr>
+        </tbody>
+    );
+};
