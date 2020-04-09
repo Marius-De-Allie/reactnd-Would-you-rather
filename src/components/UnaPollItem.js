51b05c18d5a4fa5bbffa590bc08e713e7e12a31e
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-08 22:15:49 -0400

    Update rendered JSX within UnaPollItem component to apply semantic ui styling

--- src/components/UnaPollItem.js
+++ src/components/UnaPollItem.js
@@ -30,20 +30,33 @@ const UnaPollItem = (props) => {
 
     return (
         props.questions[props.pollid] === undefined ? <NotfoundPage /> :
-        <div>
-            <h3>Would you Rather</h3>
-            <p>{props.users[props.questions[props.pollid].author].name} asks:</p>
-            <form onSubmit={onFormSubmit}>
-                <div>
-                    <input type="radio" id="opt-one" name="poll" value="optionOne" />
-                    <label htmlFor="opt-one">{props.questions[props.pollid].optionOne.text}</label>
+        <div className="ui centered card">
+            <div className="content">
+                <img className="ui avatar" src={props.users[props.questions[props.pollid].author].avatarUrl} />
+                {props.users[props.questions[props.pollid].author].name} asks:
+            </div>
+            <div className="content">
+                <h3>Would you Rather</h3>
+                <div className="description">
+                    <form className="ui form" onSubmit={onFormSubmit}>
+                        <div className="grouped fields">
+                            <div className="field">
+                                <div className="ui radio checkbox">
+                                    <input type="radio" id="opt-one" name="poll" value="optionOne" />
+                                    <label htmlFor="opt-one">{props.questions[props.pollid].optionOne.text}</label>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <div className="ui radio checkbox">
+                                    <input type="radio" id="opt-two" name="poll" value="optionTwo" />
+                                    <label htmlFor="opt-two">{props.questions[props.pollid].optionTwo.text}</label>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="fluid ui blue button" type="submit">Submit</button>
+                    </form>
                 </div>
-                <div>
-                    <input type="radio" id="opt-two" name="poll" value="optionTwo" />
-                    <label htmlFor="opt-two">{props.questions[props.pollid].optionTwo.text}</label>
-                </div>
-                <input type="submit" />
-            </form>
+            </div>
         </div>
     );
 };
