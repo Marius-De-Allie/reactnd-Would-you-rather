7a8fe294a5f76f5ab1288f45b804260862420cf5
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-10 15:43:18 -0400

    Add new className property to Link component within NotfoundPage component, set property value to ui blue button

--- src/components/NotfoundPage.js
+++ src/components/NotfoundPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotfoundPage = () => (
     <div>
         <h3>404 Page not found!</h3>
-        <p>Return<Link to="/"> Home</Link></p>
+        <Link className="ui blue button" to="/">Return Home</Link>
     </div>
 );
 
