a904f710f0c753c8d4f57abb25ea75aea3837e91
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-10 01:57:12 -0400

    Import questionItem.css stylesheet into '/src/components/QuestionItem.js' file

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+// Import stylesheet
+import '../styles/components/questionItem.css';
 
 const QuestionItem = (props) => {
     return (
