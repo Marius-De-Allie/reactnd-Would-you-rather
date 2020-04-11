b7b000769fb3a7d9324f636d3711440b94a07d9d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-11 19:00:12 -0400

    Return array of sorted question ids for all questions answered by currently authenticated user

--- src/components/AnsweredList.js
+++ src/components/AnsweredList.js
@@ -5,6 +5,8 @@ import QuestionsItem from './QuestionItem';
 const AnsweredList = (props) => {
     // Array of question ids for all questions answered by currently authenticated user.
     const authedUserAnswerIds = Object.keys(props.users[props.authedUser].answers);
+    // Sorted array of all questions answered by currently authenticated user.
+    const answerIds = props.allQuestionIds.filter(questionId => authedUserAnswerIds.includes(questionId));
     return (
         <React.Fragment>
             <h1>Answered Questions</h1>
