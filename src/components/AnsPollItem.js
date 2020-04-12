99977d7bd3bda54dcc57107903ea9f204f3da6e6
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-12 00:45:23 -0400

    Add alt property to img element within AnsPollItem component, property value is set to author's name avatar

--- src/components/AnsPollItem.js
+++ src/components/AnsPollItem.js
@@ -23,7 +23,8 @@ const AnsPollItem = (props) => {
                         <div className="content" id="avatar">
                             <img 
                                 className="ui avatar image" 
-                                src={'/' + props.users[props.questions[props.pollid].author].avatarURL} 
+                                src={'/' + props.users[props.questions[props.pollid].author].avatarURL}
+                                alt={`${props.users[props.questions[props.pollid].author].name}'s avatar`} 
                             />
                             {users[questions[pollid].author].name} asked:
                         </div>
