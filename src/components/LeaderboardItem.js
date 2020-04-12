4a18c783b7f3699c001a948f502ab7d742eda35f
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-12 00:46:39 -0400

    Add alt property to img element within LeaderboardItem component, property value is set to author's name avatar

--- src/components/LeaderboardItem.js
+++ src/components/LeaderboardItem.js
@@ -7,7 +7,11 @@ const LeaderboardItem = props => {
             <tr>
                 <td>
                     <h4 className="ui image header">
-                        <img className="ui avatar image" src={'/' + props.users[props.id].avatarURL} />
+                        <img 
+                            className="ui avatar image" 
+                            src={'/' + props.users[props.id].avatarURL} 
+                            alt={`${props.users[props.id].name}'s avatar`}
+                        />
                         <div className="content">
                             {props.users[props.id].name}
                         </div>
