949c1358e5d47e9c7e6e5ad2ee4d704d15770ee9
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-12 20:48:39 -0400

    Replace ADD_VOTE case in questions reducer with ADD_ANSWER case

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -6,7 +6,7 @@ const questions = (state = {}, action) => {
     switch(action.type) {
         case RECEIVE_QUESTIONS:
             return {...state, ...action.questions}
-        case ADD_VOTE:
+        case ADD_ANSWER:
             return {
                 ...state,
                 [action.qid] : {
