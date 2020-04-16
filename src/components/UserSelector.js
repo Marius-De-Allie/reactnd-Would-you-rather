e91dd87f891727caf9eb96987566d0a703587714
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-16 18:51:00 -0400

    Update value property on first option element in UserSelector component from sarahedo to janedoe, also update text content of 1st option element to Jane Doe

--- src/components/UserSelector.js
+++ src/components/UserSelector.js
@@ -22,7 +22,7 @@ const UserSelector = ({setAuthedUser}) => {
                 onChange={onChangeHandler}
             >
                 <option disabled value="">--Select a user to login--</option>
-                <option value="sarahedo">Sarah Edo</option>
+                <option value="janedoe">Jane Doe</option>
                 <option value="tylermcginnis">Tyler McGinnis</option>
                 <option value="johndoe">John Doe</option>
             </select>
