92ad8a32eefdbf814065ab587ea3d0a8a1784247
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-17 17:38:06 -0400

    Create production build

---
+++ build/precache-manifest.a8cf28836d22a14911cda0c5c7f96230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaa6bdcd715bbdd9a56584adc19bc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a52b33a88bb96d8d0835",
+    "url": "/static/css/main.ac6fc283.chunk.css"
+  },
+  {
+    "revision": "76aa75021a40c353a2b7",
+    "url": "/static/js/2.b049ef6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b049ef6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52b33a88bb96d8d0835",
+    "url": "/static/js/main.cd3268eb.chunk.js"
+  },
+  {
+    "revision": "08eab15d6fe4998d3d31",
+    "url": "/static/js/runtime-main.0512fb89.js"
+  }
+]);
